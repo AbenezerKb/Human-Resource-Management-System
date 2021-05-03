c2bdf8a6c99f3720e985c14014d3a2806c523b61
M0amin <60422727+M0amin@users.noreply.github.com>
2021-05-03 16:33:05 +0300

    comm123

--- hr/management/Search.java
+++ hr/management/Search.java
@@ -10,13 +10,34 @@ public class Search {
   
     
     
-    public static Employee searchEmployee(String ID) throws Exception{
-    ResultSet set = DbConnection.stmt.executeQuery("SELECT * FROM employee WHERE ID = " + ID);
-        
-    Employee emp = new Employee( set.getString("ID"), set.getString("firstname"),set.getString("lastname"),set.getString("sex"),set.getString("address"),set.getInt("age"),set.getString("role"));   
-    return emp;
+    public Employee searchEmployee(String ID) {
+        Employee emp=null;
+        try{
+    ResultSet set = DbConnection.stmt.executeQuery("SELECT * FROM employee WHERE employee_id = '" + ID +"'");
+     while(set.next()){
+       emp = new Employee(set.getString(1),set.getString(2),set.getString(3),set.getString(4),set.getString(5),set.getInt(6),set.getString(7));   
+     }
+        }catch(SQLException e)
+        {
+            e.printStackTrace();
+        }
+       return emp;
     }
- 
+    public Employee getAllEmployee() {
+        Employee emp=null;
+        try{
+    ResultSet set = DbConnection.stmt.executeQuery("SELECT * FROM employee");
+    
+     while(set.next()){
+         
+       emp = new Employee(set.getString(1),set.getString(2),set.getString(3),set.getString(4),set.getString(5),set.getInt(6),set.getString(7));   
+     }
+        }catch(SQLException e)
+        {
+            e.printStackTrace();
+        }
+       return emp;
+    }
     
     
       public static Attendance searchAttendance(String ID) throws Exception{
